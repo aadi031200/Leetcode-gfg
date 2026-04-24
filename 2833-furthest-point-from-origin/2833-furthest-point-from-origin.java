370dafc429595d996b42aa050283b57ab6d5fad8
Aditya Chourasiya <aaadi0799@gmail.com>
2026-04-24 23:09:01 +0530

    Time: 1 ms (100%), Space: 43.9 MB (45.71%) - LeetHub

--- 2833-furthest-point-from-origin/2833-furthest-point-from-origin.java
+++ 2833-furthest-point-from-origin/2833-furthest-point-from-origin.java
@@ -1,23 +1,27 @@
 class Solution {
     public int furthestDistanceFromOrigin(String moves) {
-        int count=0;
-        int l=0;
-        int r=0;
-        for(int i=0;i<moves.length();i++){
-            if(moves.charAt(i)=='L') l++;
-            else if(moves.charAt(i)=='R') r++;
+        int count = 0;
+        int l = 0;
+        int r = 0;
+        for (int i = 0; i < moves.length(); i++) {
+            if (moves.charAt(i) == 'L')
+                l++;
+            else if (moves.charAt(i) == 'R')
+                r++;
         }
-        for(int i=0;i<moves.length();i++){
-            if(l>r){
-                if(moves.charAt(i)=='L' || moves.charAt(i)=='_'){
+        if (l > r) {
+            for (int i = 0; i < moves.length(); i++) {
+                if (moves.charAt(i) == 'L' || moves.charAt(i) == '_') {
                     count--;
-                }else{ //moves.charAt(i)=='R'
+                } else { //moves.charAt(i)=='R'
                     count++;
                 }
-            }else{
-                 if(moves.charAt(i)=='R' || moves.charAt(i)=='_'){
+            }
+        } else {
+            for (int i = 0; i < moves.length(); i++) {
+                if (moves.charAt(i) == 'R' || moves.charAt(i) == '_') {
                     count++;
-                }else{ //moves.charAt(i)=='L'
+                } else { //moves.charAt(i)=='L'
                     count--;
                 }
             }
