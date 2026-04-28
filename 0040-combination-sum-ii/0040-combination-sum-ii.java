9b82e63cd6ed14019951247bd82adb800c527137
Aditya Chourasiya <aaadi0799@gmail.com>
2026-04-28 23:11:33 +0530

    Time: 6 ms (72.53%), Space: 50.6 MB (5.78%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,39 +1,24 @@
-import java.util.*;
-
 class Solution {
-
-    public void solve(int idx, int[] cand, int target,
-                      List<List<Integer>> ans,
-                      List<Integer> ds) {
-
-        if (target == 0) {
+    
+    private void findcomb(int idx,int target,int[] arr, List<List<Integer>> ans,List<Integer> ds){
+        if(target==0){
             ans.add(new ArrayList<>(ds));
             return;
         }
+        for(int i=idx;i<arr.length;i++){
+            if(i>idx && arr[i]==arr[i-1]) continue;
+            if(arr[i]>target) break;
 
-        for (int i = idx; i < cand.length; i++) {
-
-            // skip duplicates
-            if (i > idx && cand[i] == cand[i - 1]) continue;
-
-            // stop if element is bigger
-            if (cand[i] > target) break;
-
-            ds.add(cand[i]);
-
-            solve(i + 1, cand, target - cand[i], ans, ds);
-
-            ds.remove(ds.size() - 1);
+            ds.add(arr[i]);
+            findcomb(i+1,target-arr[i],arr,ans,ds);
+            ds.remove(ds.size()-1);
         }
     }
 
     public List<List<Integer>> combinationSum2(int[] cand, int target) {
-
+        List<List<Integer>> ans= new ArrayList<>();
         Arrays.sort(cand);
-
-        List<List<Integer>> ans = new ArrayList<>();
-
-        solve(0, cand, target, ans, new ArrayList<>());
+        findcomb(0,target,cand,ans,new ArrayList<>());
 
         return ans;
     }
