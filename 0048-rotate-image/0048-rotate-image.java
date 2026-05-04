43d55288440dff4bf753dff918924b3db140a964
Aditya Chourasiya <aaadi0799@gmail.com>
2026-05-04 07:50:59 +0530

    Time: 0 ms (100%), Space: 43.6 MB (69.68%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,26 @@
+class Solution {
+    public void rotate(int[][] arr) {
+        int m=arr.length;
+        //taking transpose
+        for(int i=0;i<m;i++){
+            for(int j=0;j<i;j++){
+                int temp=arr[i][j];
+                arr[i][j]=arr[j][i];
+                arr[j][i]=temp;
+            }
+        }
+        //reversing each row
+        for(int i=0;i<m;i++){
+            int a=0;
+            int b=m-1;
+            while(a<b){
+                int temp=arr[i][a];
+                arr[i][a]=arr[i][b];
+                arr[i][b]=temp;
+                a++;
+                b--;
+            }
+        }
+
+    }
+}
