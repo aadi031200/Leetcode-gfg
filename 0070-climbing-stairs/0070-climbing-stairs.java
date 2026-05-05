5565047a6f577587cdc92cb85cf02485e4e59c0c
Aditya Chourasiya <aaadi0799@gmail.com>
2026-05-05 17:49:16 +0530

    Time: 0 ms (100%), Space: 41.9 MB (72.32%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,16 +1,40 @@
 class Solution {
+
+    //USING MEMOIZATION
+
+    // public int climbStairs(int n) {
+
+    //     int[] dp= new int[n+1];
+    //     Arrays.fill(dp,-1);
+    //     return climb(n,dp);
+    // }
+
+    // private int climb(int n,int[] dp){
+    //     if(n==0 || n==1) return 1;
+
+    //     if(dp[n]!=-1) return dp[n];
+
+    //     return dp[n]=climb(n-1,dp)+climb(n-2,dp);
+    // }
+    //--------------------------------------------------------------------------------------------------------------
+    // USING TABULATION
+
     public int climbStairs(int n) {
-       
-        int[] dp= new int[n+1];
-        Arrays.fill(dp,-1);
-        return climb(n,dp);
+        return climb(n);
     }
 
-    private int climb(int n,int[] dp){
-        if(n==0 || n==1) return 1;
+    private int climb(int n) {
+        if(n<=1) return 1;
 
-        if(dp[n]!=-1) return dp[n];
+        int prev = 1;
+        int prev2 = 1;
+        for (int i = 2; i <= n; i++) {
+            int curr = prev + prev2;
+            prev2 = prev;
+            prev = curr;
+        }
 
-        return dp[n]=climb(n-1,dp)+climb(n-2,dp);
+        return prev;
     }
+
 }
