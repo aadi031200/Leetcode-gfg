98fe81eade71d5d8dac0b6bb3b068f28e49cc613
Aditya Chourasiya <aaadi0799@gmail.com>
2026-05-14 19:50:35 +0530

    Time: 1 ms (91.05%), Space: 44.5 MB (86.29%) - LeetHub

--- 2784-check-if-array-is-good/2784-check-if-array-is-good.java
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.java
@@ -1,12 +1,29 @@
 class Solution {
     public boolean isGood(int[] nums) {
-        int n=nums.length;
-        Arrays.sort(nums);
-        if(nums[n-1]!= n-1 || nums[n-2]!=n-1) return false;
+        // int n=nums.length;
+        // Arrays.sort(nums);
+        // if(nums[n-1]!= n-1 || nums[n-2]!=n-1) return false;
 
-        for(int i=0;i<=n-2;i++){
-            if(nums[i]!=i+1) return false;
+        // for(int i=0;i<=n-2;i++){
+        //     if(nums[i]!=i+1) return false;
+        // }
+        // return true;
+
+        int n=nums.length-1;
+        int[] freq= new int[nums.length-1];
+        
+        for(int number:nums){
+            if(number>n) return false;
+
+            freq[number-1]++;
         }
-        return true;
+
+        for(int i=0;i<n-1;i++){
+            if(freq[i]!=1) return false;
+
+            if(freq[n-1]!=2) return false;
+        }
+    return true;
+
     }
 }
