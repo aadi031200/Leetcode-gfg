ab809d805884c4ca0a8eab90ea57b323d9c8db93
Aditya Chourasiya <aaadi0799@gmail.com>
2026-05-19 21:09:11 +0530

    Time: 2 ms (88.98%), Space: 80.4 MB (48.43%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.java
+++ 2540-minimum-common-value/2540-minimum-common-value.java
@@ -1,20 +1,45 @@
+// class Solution {
+//     public int getCommon(int[] nums1, int[] nums2) {
+
+//         HashSet<Integer> set = new HashSet<>();
+
+//         for (int num : nums1) {
+//             set.add(num);
+//         }
+
+//         int ans = Integer.MAX_VALUE;
+
+//         for (int num : nums2) {
+//             if (set.contains(num)) {
+//                 ans = Math.min(ans, num);
+//             }
+//         }
+
+//         return ans == Integer.MAX_VALUE ? -1 : ans;
+//     }
+// }
+
+
 class Solution {
     public int getCommon(int[] nums1, int[] nums2) {
+        int i = 0;
+        int j = 0;
 
-        HashSet<Integer> set = new HashSet<>();
+        while (i < nums1.length && j < nums2.length) {
 
-        for (int num : nums1) {
-            set.add(num);
-        }
+            if (nums1[i] == nums2[j]) {
+                return nums1[i];
+            }
 
-        int ans = Integer.MAX_VALUE;
+            else if (nums1[i] < nums2[j]) {
+                i++;
+            }
 
-        for (int num : nums2) {
-            if (set.contains(num)) {
-                ans = Math.min(ans, num);
+            else {
+                j++;
             }
         }
 
-        return ans == Integer.MAX_VALUE ? -1 : ans;
+        return -1;
     }
 }
