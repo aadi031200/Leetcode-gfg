0ee9c3793d745daf1350af7ee6c5323267a5b3ce
Aditya Chourasiya <aaadi0799@gmail.com>
2026-05-21 20:57:31 +0530

    Time: 1 ms (97.99%), Space: 40.8 MB (99.97%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int mySqrt(int x) {
+        long low=1, high=(long)x;
+        while(low<=high){
+            long mid=(low+high)/2;
+            if(mid*mid==x)  return (int)mid;
+            else if(mid*mid>=x) high=mid-1;
+            else low=mid+1;
+        }
+        return (int)high;
+    }
+}
